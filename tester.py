2be1118347408fa894f06c4b5945869ac72952cb
Roman Teslenko <rteslenko@37bytes.com>
2022-05-14 15:25:05 +0500

    улучшение алгоритма и механизма тестирования истории: - определение победителя в свече на основе соотношения лонгистов/шортистов и максимального объема - запуск анализа точек входа по завершению очередной минутной свечи - подтверждение касания уровня и точки входа

--- tester.py
+++ tester.py
@@ -1,6 +1,12 @@
 import csv
 import datetime
+
 import pandas as pd
+from tinkoff.invest import TradeDirection
+
+from finplot_graph import FinplotGraph
+from settings import CURRENT_TIMEFRAME, FIRST_TOUCH_VOLUME_LEVEL, SECOND_TOUCH_VOLUME_LEVEL
+from utils.utils import Utils
 
 pd.options.display.max_columns = None
 pd.options.display.max_rows = None
@@ -12,92 +18,155 @@ TIMEFRAME = {
     '30min': 30,
     '1h': 60
 }
-CURRENT_TIMEFRAME = '1h'
-
-# первое касание объемного уровня интересно через Х минут
-FIRST_TOUCH_VOLUME_LEVEL = 90
-
-# последующие касание объемного уровня интересно через Х минут
-SECOND_TOUCH_VOLUME_LEVEL = 30
-
-# на сколько % выше или ниже цена может подойти к объемному уровню и его не каснуться
-PERCENTAGE_VOLUME_LEVEL_RANGE = 0.03
 
 
-def main():
-    df = pd.DataFrame(columns=['figi', 'direction', 'price', 'quantity', 'time'])
-    fix_date = {}
-    clusters = None
-    processed_volume_levels = {}
+def processed_volume_levels_to_times(processed_volume_levels):
+    times = []
+    if processed_volume_levels is None:
+        return times
 
-    with open('./data/statistics-20220428.csv', newline='') as file:
-        reader = csv.DictReader(file, delimiter=',')
-        for row in reader:
-            price = float(row['price'])
-            time = datetime.datetime.strptime(row['time'], "%Y-%m-%d %H:%M:%S.%f%z")
+    for price, volume_level in processed_volume_levels.items():
+        times += volume_level['times']
+    return times
 
-            # пропускаю премаркет
-            if time.hour < 7:
-                continue
 
-            if CURRENT_TIMEFRAME not in fix_date:
-                fix_date[CURRENT_TIMEFRAME] = time.hour
+def apply_frame_type(df):
+    return df.astype({
+        'figi': 'object',
+        'direction': 'int64',
+        'price': 'float64',
+        'quantity': 'int64',
+        # 'time': 'datetime64[ms]',
+    })
 
-            if fix_date[CURRENT_TIMEFRAME] < time.hour:
-                fix_date[CURRENT_TIMEFRAME] = time.hour
-                clusters = to_cluster(df, CURRENT_TIMEFRAME)
 
-            data = pd.DataFrame.from_records([
-                {
-                    'figi': row['figi'],
-                    'direction': row['direction'],
-                    'price': price,
-                    'quantity': row['quantity'],
-                    'time': time,
-                }
-            ])
-            df = pd.concat([df, data])
+class Tester:
+    def __init__(self):
+        self.df = pd.DataFrame(columns=['figi', 'direction', 'price', 'quantity', 'time'])
+        self.df = apply_frame_type(self.df)
 
-            if clusters is not None:
-                for index, cluster in clusters.iterrows():
-                    level_range = cluster.price * PERCENTAGE_VOLUME_LEVEL_RANGE / 100
-                    increased_level = cluster.price + level_range
-                    reduced_level = cluster.price - level_range
-                    if reduced_level <= price <= increased_level:
-                        timedelta = time - cluster.time
-                        if timedelta < datetime.timedelta(minutes=FIRST_TOUCH_VOLUME_LEVEL):
-                            continue
+        self.first_tick_time = None
+        self.fix_date = {}
+        self.clusters = None
+        self.processed_volume_levels = {}
+        self.level_response = []
 
-                        if cluster.price not in processed_volume_levels:
-                            processed_volume_levels[cluster.price] = {
-                                'count_touches': 1,
-                                'last_touch_date': time
-                            }
-                        else:
-                            timedelta = time - processed_volume_levels[cluster.price]['last_touch_date']
-                            if timedelta < datetime.timedelta(minutes=SECOND_TOUCH_VOLUME_LEVEL):
+        self.finplot_graph = FinplotGraph()
+        self.finplot_graph.start()
+
+    def run(self):
+        test_start_time = datetime.datetime.now()
+        with open('./data/USD000UTSTOM-20220506-merge.csv', newline='') as file:
+            reader = csv.DictReader(file, delimiter=',')
+            for row in reader:
+                price = float(row['price'])
+                time = datetime.datetime.strptime(row['time'], "%Y-%m-%d %H:%M:%S.%f%z")
+
+                # пропускаю премаркет
+                if time.hour < 7:
+                    continue
+
+                if CURRENT_TIMEFRAME not in self.fix_date:
+                    self.fix_date[CURRENT_TIMEFRAME] = time.hour
+
+                if self.first_tick_time is None:
+                    # сбрасываю секунды, чтобы сравнивать "целые" минутные свечи
+                    self.first_tick_time = time.replace(second=0, microsecond=0)
+
+                if self.fix_date[CURRENT_TIMEFRAME] < time.hour:
+                    # построение кластерных свечей и графика раз в 1 час
+                    self.fix_date[CURRENT_TIMEFRAME] = time.hour
+                    self.clusters = Utils.ticks_to_cluster(self.df, period=CURRENT_TIMEFRAME)
+
+                    # times = processed_volume_levels_to_times(self.processed_volume_levels)
+                    # self.finplot_graph.render(self.df, entry_point_times=times, clusters=self.clusters)
+
+                data = pd.DataFrame.from_records([
+                    {
+                        'figi': row['figi'],
+                        'direction': row['direction'],
+                        'price': price,
+                        'quantity': row['quantity'],
+                        'time': time,
+                    }
+                ])
+                data = apply_frame_type(data)
+                self.df = pd.concat([self.df, data])
+
+                if (time - self.first_tick_time).total_seconds() >= 60:
+                    # сбрасываю секунды, чтобы сравнивать "целые" минутные свечи
+                    self.first_tick_time = time.replace(second=0, microsecond=0)
+                    # каждую завершенную минуту проверяю кластера на возможную ТВ
+                    if len(self.processed_volume_levels) > 0:
+                        for volume_price, volume_level in self.processed_volume_levels.items():
+                            for touch_time, value in volume_level['times'].items():
+                                if value is not None:
+                                    continue
+                                candles = Utils.ticks_to_cluster(self.df, period='1min')
+                                candles = Utils.calculate_ratio(candles)
+                                candle = candles.loc[candles['time'] == touch_time.replace(second=0, microsecond=0)]
+                                if candle is None:
+                                    print('свеча не найдена')
+                                    continue
+
+                                if candle.iloc[0]['win'] is True:
+                                    self.processed_volume_levels[volume_price]['times'][touch_time] = True
+                                    if candle.iloc[0]['direction'] == TradeDirection.TRADE_DIRECTION_BUY:
+                                        print(f'подтверждена точка входа в лонг', candle)
+                                    else:
+                                        print(f'подтверждена точка входа в шорт', candle)
+                                else:
+                                    self.processed_volume_levels[volume_price]['times'][touch_time] = False
+                                    self.processed_volume_levels[volume_price]['last_touch_time'] = None
+                                    print(f'ТВ не подходит для входа', candle)
+
+                if self.clusters is not None:
+                    for index, cluster in self.clusters.iterrows():
+                        cluster_time = cluster['time']
+                        cluster_price = cluster['max_volume_price']
+
+                        # цена может коснуться объемного уровня в заданном процентном диапазоне
+                        is_price_in_range = Utils.is_price_in_range_cluster(price, cluster_price)
+                        if is_price_in_range:
+                            timedelta = time - cluster_time
+                            if timedelta < datetime.timedelta(minutes=FIRST_TOUCH_VOLUME_LEVEL):
                                 continue
 
-                            processed_volume_levels[cluster.price] = {
-                                'count_touches': processed_volume_levels[cluster.price]['count_touches'] + 1,
-                                'last_touch_date': time
-                            }
+                            if cluster_price not in self.processed_volume_levels:
+                                # инициализация первого касания уровня
+                                self.processed_volume_levels[cluster_price] = {}
+                                self.processed_volume_levels[cluster_price]['count_touches'] = 0
+                                self.processed_volume_levels[cluster_price]['times'] = {}
+                            else:
+                                # обработка второго и последующего касания уровня на основе времени последнего касания
+                                if self.processed_volume_levels[cluster_price]['last_touch_time'] is not None:
+                                    timedelta = time - self.processed_volume_levels[cluster_price]['last_touch_time']
+                                    if timedelta < datetime.timedelta(minutes=SECOND_TOUCH_VOLUME_LEVEL):
+                                        continue
 
-                        print(f'объемный уровень {cluster.price} сформирован в {cluster.time}')
-                        print(
-                            f'{time}: цена {price} подошла к объемному уровню {processed_volume_levels[cluster.price]["count_touches"]} раз\n')
-                        break
+                            # установка параметров при касании уровня
+                            self.processed_volume_levels[cluster_price]['count_touches'] += 1
+                            self.processed_volume_levels[cluster_price]['last_touch_time'] = time
+                            self.processed_volume_levels[cluster_price]['times'][time] = None
 
+                            print(f'объемный уровень {cluster_price} сформирован в {cluster_time}', flush=True)
+                            print(
+                                f'{time}: цена {price} подошла к объемному уровню {self.processed_volume_levels[cluster_price]["count_touches"]} раз\n',
+                                flush=True)
+                            break
 
-def to_cluster(df, timeframe='5min'):
-    # df.time = pd.to_datetime(df.time, utc=True)
-    df.price = pd.to_numeric(df.price)
-    df.quantity = pd.to_numeric(df.quantity)
+        # по завершению анализа перестраиваю показания, т.к. закрытие торгов не совпадает целому часу
+        # например 15:59:59.230333+00:00
+        self.clusters = Utils.ticks_to_cluster(self.df, period=CURRENT_TIMEFRAME)
+        # times = processed_volume_levels_to_times(self.processed_volume_levels)
+        # self.finplot_graph.render(self.df, entry_point_times=times, clusters=self.clusters)
 
-    time_quantity = df.groupby([pd.Grouper(key='time', freq=timeframe), 'price'])[['quantity']].sum()
-    result = time_quantity.reset_index(level='time').groupby('time')['quantity'].idxmax().reset_index(name='price')
-    return result
+        test_end_time = datetime.datetime.now()
+        print('\nанализ завершен')
+        print(f'время тестирования: {(test_end_time - test_start_time).total_seconds()} сек.')
+        print(f'кластера: {self.clusters}')
 
 
 if __name__ == "__main__":
-    main()
+    tester = Tester()
+    tester.run()
--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,9 @@
+import numpy as np
 import pandas as pd
-from tinkoff.invest import Quotation
+from tinkoff.invest import Quotation, TradeDirection
 from tinkoff.invest.utils import quotation_to_decimal
+
+from settings import PERCENTAGE_VOLUME_LEVEL_RANGE
 
 
 class Utils:
@@ -17,16 +20,78 @@ class Utils:
         # endregion
 
     @staticmethod
+    def is_price_in_range_cluster(current_price, cluster_price):
+        level_range = cluster_price * PERCENTAGE_VOLUME_LEVEL_RANGE / 100
+        increased_level = cluster_price + level_range
+        reduced_level = cluster_price - level_range
+        return reduced_level <= current_price <= increased_level
+
+    @staticmethod
     def merge_two_frames(source_df, new_df):
         if new_df is None or len(new_df) == 0:
             return source_df
         if source_df is None or len(source_df) == 0:
             return new_df
 
-        first_time = new_df.iloc[0]['time']
-        last_time = new_df.iloc[-1]['time']
+        first_time = pd.to_datetime(new_df.iloc[0]['time'], utc=True)
+        last_time = pd.to_datetime(new_df.iloc[-1]['time'], utc=True)
         search_condition = (source_df['time'] >= first_time) & (source_df['time'] <= last_time)
 
         result_df = source_df.drop(source_df.loc[search_condition].index)
         result_df = pd.concat([result_df, new_df]).rename_axis('index')
         return result_df.sort_values(['time', 'index']).reset_index(drop=True)
+
+    @staticmethod
+    def agg_ohlc(df):
+        price = df['price'].values
+        quantity = df['quantity'].values
+        names = {
+            'low': min(price) if len(price) > 0 else np.nan,
+            'high': max(price) if len(price) > 0 else np.nan,
+            'open': price[0] if len(price) > 0 else np.nan,
+            'close': price[-1] if len(price) > 0 else np.nan,
+            'total_volume': sum(quantity) if len(quantity) > 0 else 0,
+            'max_volume_price': df.groupby(['price'])[['quantity']].sum().idxmax()[0]
+        }
+        return pd.Series(names)
+
+    @staticmethod
+    def calculate_ratio(candles):
+        # процентное соотношение лонгистов/шортистов в свече
+        difference = candles['high'] - candles['low']
+        long_ratio = (candles['close'] - candles['low']) / difference * 100
+        short_ratio = (candles['high'] - candles['close']) / difference * 100
+        candles['long'] = long_ratio
+        candles['short'] = short_ratio
+
+        # расчет расположения макс. объема относительно открытия свечи
+        from_high = abs(candles['high'] - candles['max_volume_price'])
+        from_low = abs(candles['max_volume_price'] - candles['low'])
+        total = from_high + from_low
+        candles.loc[candles['direction'] == TradeDirection.TRADE_DIRECTION_BUY, 'percent'] = from_low / total * 100
+        candles.loc[candles['direction'] == TradeDirection.TRADE_DIRECTION_SELL, 'percent'] = from_high / total * 100
+
+        # определение победителя:
+        # если соотношение лонгистов больше и макс объем как можно ниже, то приоритет для лонга
+        # если соотношение шортистов больше и макс объем как можно выше, то приоритет для шорта
+        candles.loc[candles['direction'] == TradeDirection.TRADE_DIRECTION_BUY, 'win'] = (candles['long'] > 50) & (candles['percent'] <= 40)
+        candles.loc[candles['direction'] == TradeDirection.TRADE_DIRECTION_SELL, 'win'] = (candles['short'] > 50) & (candles['percent'] <= 40)
+
+        return candles
+
+    @staticmethod
+    def ticks_to_cluster(df, period='1min'):
+        candles = df.set_index(['time'])
+        candles = candles.resample(period).apply(Utils.agg_ohlc)
+        candles = candles.ffill()
+
+        candles['time'] = candles.index
+        # свеча доджи
+        candles.loc[candles['close'] == candles['open'], 'direction'] = TradeDirection.TRADE_DIRECTION_UNSPECIFIED
+        # бычья свеча
+        candles.loc[candles['close'] > candles['open'], 'direction'] = TradeDirection.TRADE_DIRECTION_BUY
+        # медвежья свеча
+        candles.loc[candles['open'] > candles['close'], 'direction'] = TradeDirection.TRADE_DIRECTION_SELL
+
+        candles = candles[['time', 'open', 'close', 'high', 'low', 'total_volume', 'direction', 'max_volume_price']]
+        return candles.reset_index(drop=True)
