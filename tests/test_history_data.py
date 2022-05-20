e700cae9a77afe419509ded987a3c3b3d241392d
Roman Teslenko <rteslenko@37bytes.com>
2022-05-20 21:30:43 +0500

    тестирование корректности полученных исторических данных

---
+++ tests/test_history_data.py
@@ -0,0 +1,29 @@
+import csv
+
+from utils.utils import Utils
+
+# проверяет корректность файлов с историческими данными на основе последовательности даты-времени
+# в случае ошибки будет выведена строка, с которой начинается сбой последовательности
+if __name__ == "__main__":
+    prev_time = None
+
+    file_paths = [
+        './data/USD000UTSTOM-20220520.csv',
+        './data/SBER-20220520.csv',
+        './data/GAZP-20220520.csv'
+    ]
+
+    for file_path in file_paths:
+        with open(file_path, newline='') as file:
+            reader = csv.DictReader(file, delimiter=',')
+            for row in reader:
+                current_price = float(row['price'])
+                time = Utils.parse_date(row['time'])
+
+                if prev_time is None:
+                    prev_time = time
+
+                if time < prev_time:
+                    print(file_path, row)
+
+                prev_time = time
